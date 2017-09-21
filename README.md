07882b84e04a1edf345df049283abb22a93f7e86
Maik Tizziani <maik.tizziani@bvb-verlag.de>
2017-09-21 17:07:04 +0200

    initial commit settings and test files

---
+++ README.md
@@ -0,0 +1,12 @@
+# TypedSource - Webpage entwicklung
+
+## Intallation
+
+open your console and run the following sequence
+
+    npm install
+    typings install dt~jquery --global --save
+
+automatic code compilation on change files
+
+    grunt
---
+++ src/start.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/index.d.ts" />
+
+
+$(document).ready(() => {
+    console.log('document is ready');
+    console.log('build done now');
+    $('.red').css('color', 'red');
+    $('.blue').css('color', 'green');
+});
